29926219d539c77c8a22d6ba25765fb62440636c
frank-grijalba <fjgrijalba@gmail.com>
2023-12-13 14:50:26 -0500

    It is possible to override a resource field to change some of its options

--- library/book/admin.py
+++ library/book/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from import_export import resources
+from import_export.fields import Field
 from book.models import Book, Author, Category
 
 
@@ -8,14 +9,24 @@ admin.site.register(Author)
 admin.site.register(Category)
 
 
+# class BookResource(resources.ModelResource):
+#     class Meta:
+#         model = Book
+#         fields = (
+#             "id",
+#             "name",
+#             "author__name",
+#         )
+
+
+# It is possible to override a resource field to change some of its options:
 class BookResource(resources.ModelResource):
+    published = Field(attribute="published", column_name="published_date")
+    # Other fields that don’t exist in the target model may be added
+    myField = Field(column_name='myownfield')
+
     class Meta:
         model = Book
-        fields = (
-            "id",
-            "name",
-            "author__name",
-        )
 
 
 class AuthorResource(resources.ModelResource):
